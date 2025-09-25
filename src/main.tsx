f5d144ce05e225bd5c8cdc576616696baafd4d8c
dyannadle <dyannadle05@gmail.com>
2025-09-25 21:11:10 +0530

    set proper posthog api_host

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,17 @@ import App from "./App.tsx";
 import { PostHogProvider } from "posthog-js/react";
 
 const options = {
-  api_host: 'https://us.i.posthog.com',
-  ui_host: 'https://us.posthog.com',
-  loaded: (posthog: any) => {
-    if (import.meta.env.DEV) posthog.debug();
-  }
+  ui_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  api_host: "/relay-ddd/",
 };
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY || 'phc_dummy_key_for_development'}
+      apiKey={
+        import.meta.env.VITE_PUBLIC_POSTHOG_KEY ||
+        "phc_dummy_key_for_development"
+      }
       options={options}
     >
       <App />
